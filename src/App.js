515a70a0e08288fa087a1c6d92ea97a205c0ba93
pamodarasangi <pamodasilva21@gmail.com>
2025-03-20 14:37:17 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { 
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +21,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
